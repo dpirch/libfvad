f5f75aa5919f7be3932083017e49cfe4ff6069d5
Daniel Pirch <dpirch@gmail.com>
2016-09-13 18:51:07 +0200

    add replacements for RTC_DCHECK* macros

--- src/common.h
+++ src/common.h
@@ -6,7 +6,13 @@
 #include <stdbool.h>
 #include <assert.h>
 
+// replacements for WebRTC's various assert-like macros
 #define COMPILE_ASSERT(expr) static_assert(expr, #expr)
+#define RTC_DCHECK(expr) assert(expr)
+#define RTC_DCHECK_GT(a,b) assert((a) > (b))
+#define RTC_DCHECK_LT(a,b) assert((a) < (b))
+#define RTC_DCHECK_LE(a,b) assert((a) <= (b))
+
 #define arraysize(a) (sizeof (a) / sizeof *(a))
 
 
