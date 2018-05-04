cf585ec1f7d4ca18e416a5ac6fe5e750dd3ab1db
Daniel Pirch <dpirch@gmail.com>
2018-05-04 23:45:07 +0200

    add 'extern "C"' to public header for C++ support

--- include/fvad.h
+++ include/fvad.h
@@ -12,6 +12,10 @@
 #ifndef FVAD_H_
 #define FVAD_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdint.h>
 #include <stddef.h>
 
@@ -85,5 +89,8 @@ int fvad_set_sample_rate(Fvad* inst, int sample_rate);
  */
 int fvad_process(Fvad* inst, const int16_t* frame, size_t length);
 
+#ifdef __cplusplus
+}
+#endif
 
 #endif  // FVAD_H_
